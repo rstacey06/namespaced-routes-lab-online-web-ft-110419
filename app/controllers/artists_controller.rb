f44f94ba70c234b8dabe5c6c1a45a9db728747d3
github username <github email address>
2020-02-03 12:57:03 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,6 +9,9 @@ class ArtistsController < ApplicationController
 
   def new
     @artist = Artist.new
+    if !Preference.last.allow_create_artists
+      redirect_to artists_path
+    end
   end
 
   def create
